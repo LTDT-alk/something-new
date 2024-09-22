472e7040bec917d850181195b0f49b0e88b146d5
LABS <129704269+LTDT-alk@users.noreply.github.com>
2024-09-22 08:37:22 +0000

    modified:   main.js 	renamed:    storage.js -> storage/JScode.js

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 function elem(id){
     return document.getElementById(id)
 }
-para = elem("text")
 
+
+para = elem("text");
+
+addEventListener("mousemove", (e) => {
+    para.innerHTML = `${e.clientX},${e.clientY}`
+})
+/*
 addEventListener("keydown",(event) => {
     para.innerHTML += ` ${event.code}`
 })
-
+*/
