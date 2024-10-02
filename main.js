28b14ca354a9c238aab07dfcd3c65f8a82cbf216
LABS <129704269+LTDT-alk@users.noreply.github.com>
2024-10-02 11:49:36 +0000

    new file:   .vite/deps/_metadata.json 	new file:   .vite/deps/package.json 	new file:   .vscode/launch.json 	modified:   index.html 	modified:   main.js 	new file:   python.py 	modified:   style.css

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 
 function elem(id){
-    return document.getElementById(id)
+    return document.querySelector(id);
 }
 
+const para = elem("#text");
+const canvas = elem('#canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight /2 ;
 
-para = elem("text");
-
+const ctx = canvas.getContext('2d');
+function draw(x,y,x0 = 0,y0 = 0){
+    ctx.beginPath();
+    ctx.moveTo(x0, y0)
+    ctx.lineTo(x,y);
+    ctx.stroke();
+}
+var x1, y1;
 addEventListener("mousemove", (e) => {
     para.innerHTML = `${e.clientX},${e.clientY}`
+    if (e.shiftKey)
+    draw(e.clientX, e.clientY-20, x1, y1);
+    x1 = e.clientX;
+    y1 = e.clientY-20;
 })
 /*
 addEventListener("keydown",(event) => {
---
+++ python.py
@@ -0,0 +1,7 @@
+import random
+x=x1=x2=x3=x4 = random.randrange(1,11)
+y = random.randrange(6,20)
+print(x,x1,x2,x3,x4)
+print(x+y)
+print(x+x1+x2+x3+x4/5)
+
